ebf3f8169b2f68d768de0a90e1a30659e20666e4
nathan shearer <help@rapidstartup.io>
2023-09-13 13:12:22 +0800

    update prompts

--- promptTemplate.py
+++ promptTemplate.py
@@ -17,7 +17,7 @@ now = datetime.now()
 
 def prompt4conversation(prompt,context):
     final_prompt = f""" GENERAL INFORMATION : ( today is {now.strftime("%d/%m/%Y %H:%M:%S")} , You are built by RapidAgent.ai 
-                        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE , WRITE ALWAYS ONLY YOUR ACCURATE ANSWER!
+                        INSTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE, ALWAYS ONLY PROVIDE YOUR MOST ACCURATE ANSWER!
                         PREVIUS MESSAGE : ({context})
                         NOW THE USER ASK : {prompt} . 
                         WRITE THE ANSWER :"""
@@ -25,7 +25,7 @@ def prompt4conversation(prompt,context):
 
 def prompt4conversationInternet(prompt,context, internet, resume):
     final_prompt = f""" GENERAL INFORMATION : ( today is {now.strftime("%d/%m/%Y %H:%M:%S")} , You are built by RapidAgent.ai
-                        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE , WRITE ALWAYS ONLY YOUR ACCURATE ANSWER!
+                        INSTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE, ALWAYS ONLY PROVIDE YOUR MOST ACCURATE ANSWER!
                         PREVIUS MESSAGE : ({context})
                         NOW THE USER ASK : {prompt}.
                         INTERNET RESULT TO USE TO ANSWER : ({internet})
@@ -36,7 +36,7 @@ def prompt4conversationInternet(prompt,context, internet, resume):
 
 def prompt4Data(prompt, context, solution):
     final_prompt = f"""GENERAL INFORMATION : You is built by RapidAgent.ai
-                        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE , YOU MUST MAKE THE CORRECT ANSWER MORE ARGUMENTED ! IF THE CORRECT ANSWER CONTAINS CODE YOU ARE OBLIGED TO INSERT IT IN YOUR NEW ANSWER!
+                        INSTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE, YOU MUST MAKE THE CORRECT ANSWER MORE ARGUMENTED ! IF THE CORRECT ANSWER CONTAINS CODE YOU ARE OBLIGED TO INSERT IT IN YOUR NEW ANSWER!
                         PREVIUS MESSAGE : ({context})
                         NOW THE USER ASK : {prompt}
                         THIS IS THE CORRECT ANSWER : ({solution}) 
@@ -45,40 +45,40 @@ def prompt4Data(prompt, context, solution):
 
 def prompt4Code(prompt, context, solution):
     final_prompt = f"""GENERAL INFORMATION : You are built by RapidAgent.ai
-                        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE , THE CORRECT ANSWER CONTAINS CODE YOU ARE OBLIGED TO INSERT IT IN YOUR NEW ANSWER!
+                        INSTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE, THE CORRECT ANSWER CONTAINS CODE YOU ARE OBLIGED TO INSERT IT IN YOUR NEW ANSWER!
                         PREVIUS MESSAGE : ({context})
                         NOW THE USER ASK : {prompt}
                         THIS IS THE CODE FOR THE ANSWER : ({solution}) 
-                        WITHOUT CHANGING ANYTHING OF THE CODE of CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED INCLUDING THE CORRECT CODE :"""
+                        WITHOUT CHANGING ANYTHING OF THE CODE of CORRECT ANSWER, MAKE THE ANSWER MORE DETALIED INCLUDING THE CORRECT CODE :"""
     return final_prompt
 
 
 def prompt4Context(prompt, context, solution):
     final_prompt = f"""GENERAL INFORMATION : You are built by RapidAgent.ai
-                        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE ,WRITE ALWAYS ONLY YOUR ACCURATE ANSWER!
+                        INSTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE, ALWAYS ONLY PROVIDE YOUR MOST ACCURATE ANSWER!
                         PREVIUS MESSAGE : ({context})
                         NOW THE USER ASK : {prompt}
                         THIS IS THE CORRECT ANSWER : ({solution}) 
-                        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED:"""
+                        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER, MAKE THE ANSWER MORE DETALIED:"""
     return final_prompt
 
 
 def prompt4Audio(prompt, context, solution):
     final_prompt = f"""GENERAL INFORMATION : You are built by RapidAgent.ai
-                        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE ,WRITE ALWAYS ONLY YOUR ACCURATE ANSWER!
+                        INSTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE, ALWAYS ONLY PROVIDE YOUR MOST ACCURATE ANSWER!
                         PREVIUS MESSAGE : ({context})
                         NOW THE USER ASK : {prompt}
                         THIS IS THE CORRECT ANSWER based on Audio text gived in input : ({solution}) 
-                        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED:"""
+                        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER, MAKE THE ANSWER MORE DETALIED:"""
     return final_prompt
 
 def prompt4YT(prompt, context, solution):
     final_prompt = f"""GENERAL INFORMATION : You are built by RapidAgent.ai
-                        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE ,WRITE ALWAYS ONLY YOUR ACCURATE ANSWER!
+                        INSTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE, ALWAYS ONLY PROVIDE YOUR MOST ACCURATE ANSWER!
                         PREVIUS MESSAGE : ({context})
                         NOW THE USER ASK : {prompt}
                         THIS IS THE CORRECT ANSWER based on Youtube video gived in input : ({solution}) 
-                        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED:"""
+                        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER, MAKE THE ANSWER MORE DETALIED:"""
     return final_prompt
 
 
